46cd19747f5eb502a733db5624ca3742385b98cb
Joshua <joshua@psychotechnology.com>
2020-03-21 07:57:54 +0000

    Sorted country list alphabetically

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import ThreeFactorChart from "./components/charts/ThreeFactorChart";
 import Histogram from "./components/charts/Histogram";
 import { LOCATION_API } from "./api/Endpoints";
+import _ from "lodash";
 
 function App() {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    fetch(LOCATION_API).then(res => res.json().then(res => setCountries(res.locations)));
+    fetch(LOCATION_API).then(res => res.json().then(res => {
+      const sortedLocations = _.sortBy(res.locations, o => _.isEmpty(o.province) ? o.country : o.province);
+      setCountries(sortedLocations);
+    }));
   }, []);
 
   return (
